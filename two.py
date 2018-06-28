822f64f6da94164525d8ab3688afa63acce9a084
arvind <aparihar790@gmail.com>
2018-06-28 21:02:48 +0530

    exmple

---
+++ two.py
@@ -0,0 +1,20 @@
+def add_friends():
+  user_name =input("what is your name")
+  user_age =int(input("what is your age"))
+  user_rating =float(input("what is your rating"))
+
+ if (user_name)>0 and user_age > 12 and usre_age < 60:
+   friends_name .append (user_name)
+   friends_age .append (user_age)
+   friends_rating .append (user_rating)
+   frineds_status.append (Ture)
+
+ else:
+   print("sorry!you have a invalid insertion")
+ return len(friends_name)
+
+ friends_name=[]
+  friends_age=[]
+  friends_rating=[]
+ friends_online=[]
+print(your friend %s friends_name[len(friends_name)-1])
