4658379acf1cb60a8904c509233755bc8d8ae9ae
arvind <aparihar790@gmail.com>
2018-07-09 18:39:49 +0530

    inharitance example

---
+++ inheri.py
@@ -0,0 +1,25 @@
+class Person:
+    def__init__(self,name,age)
+    self.name=name
+    self.age=age
+      print "calling person"
+     def showName(self):
+        print(self.name)
+
+     def showAge(self):
+        print(self.age)
+
+class Student(Person):
+    def__init__(self,name,age,roll)
+    person.__init__(self,name,age)
+    self.roll=roll
+    def getroll(self):
+        return(self.roll)
+
+P1=Person('tommy',21)
+P1.showName()
+P1.showage()
+S=Student('tommy',21,2014)
+s.Showname()
+s.ShowAge()
+print(s.getroll())
